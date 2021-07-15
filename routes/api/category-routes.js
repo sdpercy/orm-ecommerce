c45efc3be95fdf5618d3e3e51615a5ce73ee88fa
sdpercy <scottpercy@hotmail.com>
2021-07-15 10:16:14 -0230

    added find one category by id route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,11 +6,42 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    attributes: ['id', 'category_name'],
+    include: [{
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }]
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: { id: req.id },
+    attributes: ['id', 'category_name'],
+    include: [{
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }]
+    })
+    .then(dbCategoryData => {
+      if (!dbCategoryData){
+        res.status(404).json({message: 'Could not find a category with this id'});
+        return;
+      }
+      res.json(dbCategoryData)
+    })
+    .catch(err => {
+      console.log(err);
+        res.status(500).json(err);
+    })
 });
 
 router.post('/', (req, res) => {
